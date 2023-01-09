7df6eced24d0eeaa07fe443dadd6030d4bede817
Viet Tran <viettran101294@gmail.com>
2023-01-09 15:23:25 -0500

    add tail function

---
+++ tail.js
@@ -0,0 +1,14 @@
+const assertEqual = (actual, expected) => {
+  actual === expected
+    ? console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`)
+    : console.log(`🛑🛑🛑 Assertion Failed: ${actual} !== ${expected}`);
+};
+
+const tail = (words) => {
+  return words.slice(1);
+};
+
+const result = tail(["Hello", "Lighthouse", "Labs"]);
+assertEqual(result.length, 2); // ensure we get back two elements
+assertEqual(result[0], "Lighthouse"); // ensure first element is "Lighthouse"
+assertEqual(result[1], "Labs"); // ensure second element is "Labs"
