0ad49ed93d6f7706d8288df70a07cac70af620bd
Viet Tran <viettran101294@gmail.com>
2023-01-10 17:08:24 -0500

    create assertArraysEqual function

---
+++ assertArraysEqual.js
@@ -0,0 +1,16 @@
+const eqArrays = (arr1, arr2) => {
+  let res = true;
+  if (arr1.length !== arr2.length) res = false;
+
+  for (let i = 0; i < arr1.length; i++) {
+    if (arr1[i] !== arr2[i]) {
+      res = false;
+      break;
+    }
+  }
+  res
+    ? console.log(`✅✅✅ Assertion Passed: ${arr1} === ${arr2}`)
+    : console.log(`🛑🛑🛑 Assertion Failed: ${arr1} !== ${arr2}`);
+};
+
+eqArrays([1, 2], [1, 2]);
