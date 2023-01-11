94e498c6a901e710e16768ec4f16d5b8a6464566
Viet Tran <viettran101294@gmail.com>
2023-01-11 13:14:40 -0500

    add middle function

---
+++ middle.js
@@ -0,0 +1,29 @@
+const eqArrays = (arr1, arr2) => {
+  if (arr1.length !== arr2.length) return false;
+
+  for (let i = 0; i < arr1.length; i++) {
+    if (arr1[i] !== arr2[i]) return false;
+  }
+  return true;
+};
+
+const assertArraysEqual = (arr1, arr2) => {
+  const res = eqArrays(arr1, arr2);
+  res
+    ? console.log(`✅✅✅ Assertion Passed: ${arr1} === ${arr2}`)
+    : console.log(`🛑🛑🛑 Assertion Failed: ${arr1} !== ${arr2}`);
+};
+
+const middle = (array) => {
+  if (array.length < 3) return [];
+  const check = array.length % 2;
+  const midInd = Math.floor(array.length / 2);
+  return check ? [array[midInd]] : [array[midInd - 1], array[midInd]];
+};
+
+assertArraysEqual(middle([1]), []);
+assertArraysEqual(middle([1, 2]), []);
+assertArraysEqual(middle([1, 2, 3]), [2]);
+assertArraysEqual(middle([1, 2, 3, 4, 5]), [3]);
+assertArraysEqual(middle([1, 2, 3, 4]), [2, 3]);
+assertArraysEqual(middle([1, 2, 3, 4, 5, 6]), [3, 4]);
