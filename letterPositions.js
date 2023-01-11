449fb456b2c8ec94af2737af18eebc7590a6bf8c
Viet Tran <viettran101294@gmail.com>
2023-01-11 15:13:48 -0500

    add letter position

---
+++ letterPositions.js
@@ -0,0 +1,34 @@
+const eqArrays = (arr1, arr2) => {
+  if (arr1.length !== arr2.length) return false;
+
+  for (let i = 0; i < arr1.length; i++) {
+    if (arr1[i] !== arr2[i]) return false;
+  }
+  return true;
+};
+
+const assertArraysEqual = (arr1, arr2) => {
+  const res = eqArrays(arr1, arr2);
+  res
+    ? console.log(`✅✅✅ Assertion Passed: ${arr1} === ${arr2}`)
+    : console.log(`🛑🛑🛑 Assertion Failed: ${arr1} !== ${arr2}`);
+};
+
+const letterPositions = (sentence) => {
+  const results = {};
+
+  for (let index in sentence) {
+    if (sentence[index] === " ") continue;
+    // results[sentence[index]] = results[sentence[index]]
+    //   ? results[sentence[index]].concat([Number(index)])
+    //   : [Number(index)];
+    results[sentence[index]] = results[sentence[index]]
+      ? [...results[sentence[index]], Number(index)]
+      : [Number(index)];
+  }
+
+  return results;
+};
+
+console.log(letterPositions("lighthouse in the house"));
+assertArraysEqual(letterPositions("hello").e, [1]);
