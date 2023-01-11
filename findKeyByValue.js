e8ce9b824fdf6544d2f40157c243fe8018d2eda2
Viet Tran <viettran101294@gmail.com>
2023-01-11 16:02:24 -0500

    create findkeybyvalue

---
+++ findKeyByValue.js
@@ -0,0 +1,23 @@
+/* eslint-disable */
+
+const assertEqual = (actual, expected) => {
+  actual === expected
+    ? console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`)
+    : console.log(`🛑🛑🛑 Assertion Failed: ${actual} !== ${expected}`);
+};
+
+const findKeyByValue = (data, title) => {
+  for (let genre of Object.keys(data)) {
+    if (data[genre] === title) return genre;
+  }
+  return undefined;
+};
+
+const bestTVShowsByGenre = {
+  sci_fi: "The Expanse",
+  comedy: "Brooklyn Nine-Nine",
+  drama: "The Wire",
+};
+
+assertEqual(findKeyByValue(bestTVShowsByGenre, "The Wire"), "drama");
+assertEqual(findKeyByValue(bestTVShowsByGenre, "That '70s Show"), undefined);
