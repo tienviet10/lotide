6a3ef71af5bb00267f375905cb0eeb78c933c538
Viet Tran <viettran101294@gmail.com>
2023-01-12 13:29:21 -0500

    add find key function

---
+++ findKey.js
@@ -0,0 +1,26 @@
+const assertEqual = (actual, expected) => {
+  actual === expected
+    ? console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`)
+    : console.log(`🛑🛑🛑 Assertion Failed: ${actual} !== ${expected}`);
+};
+
+const findKey = (data, cond) => {
+  for (let name in data) {
+    if (cond(data[name])) return name;
+  }
+  return undefined;
+};
+
+const result = findKey(
+  {
+    "Blue Hill": { stars: 1 },
+    Akaleri: { stars: 3 },
+    noma: { stars: 2 },
+    elBulli: { stars: 3 },
+    Ora: { stars: 2 },
+    Akelarre: { stars: 3 },
+  },
+  (x) => x.stars === 2
+);
+
+assertEqual(result, "noma");
