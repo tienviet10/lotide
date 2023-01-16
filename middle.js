2a5e1bf18d919bf5d16201dda9e79607750cd00e
Viet Tran <viettran101294@gmail.com>
2023-01-16 09:49:39 -0500

    refactor middle

--- middle.js
+++ middle.js
@@ -1,19 +1,3 @@
-const eqArrays = (arr1, arr2) => {
-  if (arr1.length !== arr2.length) return false;
-
-  for (let i = 0; i < arr1.length; i++) {
-    if (arr1[i] !== arr2[i]) return false;
-  }
-  return true;
-};
-
-const assertArraysEqual = (arr1, arr2) => {
-  const res = eqArrays(arr1, arr2);
-  res
-    ? console.log(`✅✅✅ Assertion Passed: ${arr1} === ${arr2}`)
-    : console.log(`🛑🛑🛑 Assertion Failed: ${arr1} !== ${arr2}`);
-};
-
 const middle = (array) => {
   if (array.length < 3) return [];
 
@@ -23,9 +7,4 @@ const middle = (array) => {
   return check ? [array[midInd]] : [array[midInd - 1], array[midInd]];
 };
 
-assertArraysEqual(middle([1]), []);
-assertArraysEqual(middle([1, 2]), []);
-assertArraysEqual(middle([1, 2, 3]), [2]);
-assertArraysEqual(middle([1, 2, 3, 4, 5]), [3]);
-assertArraysEqual(middle([1, 2, 3, 4]), [2, 3]);
-assertArraysEqual(middle([1, 2, 3, 4, 5, 6]), [3, 4]);
+module.exports = middle;
---
+++ test/assertArraysEqualTest.js
@@ -0,0 +1,3 @@
+const assertArraysEqual = require('../assertArraysEqual');
+
+assertArraysEqual([1, 2], [1, 2]);
