94e4a22a4b536464953dcd5295f6539f8662857a
Viet Tran <viettran101294@gmail.com>
2023-01-16 10:33:56 -0500

    refactoring tail test

--- test/tailTest.js
+++ test/tailTest.js
@@ -1,7 +1,20 @@
-const assertEqual = require('../assertEqual');
+const assert = require('chai').assert;
+// const assertEqual = require('../assertEqual');
 const tail = require('../tail.js');
 
 const result = tail(["Hello", "Lighthouse", "Labs"]);
-assertEqual(result.length, 2); // ensure we get back two elements
-assertEqual(result[0], "Lighthouse"); // ensure first element is "Lighthouse"
-assertEqual(result[1], "Labs"); // ensure second element is "Labs"
+// assertEqual(result.length, 2); // ensure we get back two elements
+// assertEqual(result[0], "Lighthouse"); // ensure first element is "Lighthouse"
+// assertEqual(result[1], "Labs"); // ensure second element is "Labs"
+
+describe("#head", () => {
+  it("returns 2 for ['Hello', 'Lighthouse', 'Labs']", () => {
+    assert.strictEqual(result.length, 2);
+  });
+  it("returns Lighthouse for ['Hello', 'Lighthouse', 'Labs']", () => {
+    assert.strictEqual(result[0], "Lighthouse");
+  });
+  it("returns Labs for ['Hello', 'Lighthouse', 'Labs']", () => {
+    assert.strictEqual(result[1], "Labs");
+  });
+});
